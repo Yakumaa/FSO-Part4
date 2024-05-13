7218e9134acaf30415de02262c59e5bbef69d850
Yakumaa <maharjanshrish8@gmail.com>
2024-05-13 19:17:50 +0545

    4.18-4.19: Add login route for user authentication

--- utils/middleware.js
+++ utils/middleware.js
@@ -23,7 +23,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: 'expected `username` to be unique' })
   } else if(error.name === 'JsonWebTokenError'){
     return response.status(401).json({ error: 'invalid token' })
-  } else if(error.name === 'ToeknExpiredError'){
+  } else if(error.name === 'TokenExpiredError'){
     return response.status(401).json({
       error: 'token expired'
     })
